8bbb445e0b0940f667fb18d96df1182067794547
JangMinWoo <cpsjk11@naver.com>
2023-05-21 04:05:01 +0900

    메타마스크 붙임

--- apps/client/src/states/web3.js
+++ apps/client/src/states/web3.js
@@ -4,6 +4,15 @@ import uuid from 'react-uuid'
 
 let web3
 let account
+const ERC1155 = "0x2351512098b103289096ED876059b079d0a8D18C"
+
+const contractAddress = '0x189B2FDF0ddaBcB95e8B271B8508411020bDbE20'
+
+async function _with_gas(transaction) {
+  const fee = await web3.eth.estimateGas(transaction);
+  const gas_price = await web3.eth.getGasPrice();
+  return Object.assign(transaction, { gasPrice: gas_price, gas: fee * 2 });
+}
 
 /** 메타마스크 로그인 함수  */
 export const metaMaskLogin = async () => {
@@ -38,6 +47,10 @@ async function getAccount() {
     })  
 }
 
+function getContract() {
+  return new web3.eth.Contract(ABI, contractAddress)
+}
+
 /** 현재 메타마스크 지갑이랑 연동되 있는 계정 주소랑 현재 파라미터로 받은 주소와 일치하는 지 확인하는 함수 */
 async function checkAccount () {
   const Account = await getAccount()
@@ -51,25 +64,61 @@ async function checkAccount () {
 function toBN(number) {
     return new web3.utils.BN(number);
 }
+// mintWithUriByTokenId(address token, uint256 tokenId, uint256 amount, uint256 signatureId, bytes memory signature)
+async function mintWithUriByTokenId(token, tokenId, amount, signatureId, signature) {
+
+  const account = await getAccount()
+
+  const contract = await getContract()
+
+  console.log('signatureId', signatureId, signature)
+
+  return await web3.eth.sendTransaction(
+    await _with_gas({
+      from: account,
+      to: contractAddress,
+      data: web3.eth.abi.encodeFunctionCall({
+          name: 'mintWithUriByTokenId',
+          type: 'function',
+          inputs: [
+              { type: 'address', name: 'token' },
+              { type: 'uint256', name: 'tokenId' },
+              { type: 'uint256', name: 'amount' },
+              { type: 'uint256', name: 'signatureId' },
+              { type: 'bytes', name: 'signature' },
+          ]
+      }, [ token, tokenId, amount, signatureId, signature ])
+    })
+  )
+
+}
 
 /** 해당 컨트랙트에 appvoe 승인 개수를 확인하는 함수 */
 export async function getAllowance() {
-    metaMaskLogin()
+    await metaMaskLogin()
     const signatureId = '0x' + uuid().replace(/-/g, '');
+    const account = await getAccount()
+    console.log('account', account)
     const message = [
         "mintWithUriByTokenId",
         toBN(String(signatureId)),
         '0x2351512098b103289096ED876059b079d0a8D18C',
-        account,
-        toBN('7'),
+        String(account),
+        toBN('10'),
         toBN('1')
     ]; 
     // console.log('signatureId', toBN(String(signatureId)))
-    const data = await web3.utils.soliditySha3({ type: 'bytes', value: encode_packed(message) })
+    const data = web3.utils.soliditySha3({ type: 'bytes', value: encode_packed(message) })
     console.log('message', data)
-    web3.eth.sign(data)
-    .then(console.log)
-    .catch(console.log)
+    web3.eth.personal.sign(data, account)
+    .then(async res => {
+      console.log(res, 'signatureId')
+        const result = await mintWithUriByTokenId(ERC1155, 10, 1, signatureId, res)
+        console.log('\r\n', result)
+      })
+    .catch(error => {
+      console.log('error', error)
+    })
     // console.log('signature', signature, signatureId)
 }
 
@@ -91,309 +140,5 @@ function encode_packed (values) {
         return "";
     }).join("");
   }
-
-/** 컨트랙트 반환 함수 */
-function getContract(ABI, contractAddress,) {
-  return new web3.eth.Contract(ABI, contractAddress)
-}
-
-/** 해당 토큰 컨트랙트애 approve 수행하는 함수 */
-export const setApprove = async (account, contractAddress, tokenAddress, amount) => {
-
-  if (! (await checkAccount())) return alert('Please check your account!')
-  
-  const contract = getContract(tokenABI, tokenAddress)
-
-  const allowance = await getAllowance(account, contractAddress, tokenAddress)
-
-  if (allowance >= amount) return alert('You already approved this amount!')
-  
-  const approveLimit = await contract.methods.setApprove(contractAddress, amount).estimateGas({ from: account })
-  
-  // address token, uint256 tokenId, uint256 amount, uint256 signatureId, bytes memory signature
-
-  await contract.methods.mintWithUriByTokenId(contractAddress, amount)
-        .send({
-          from: account,
-          gas: approveLimit
-        })
-        .on('transactionHash', (hash) => {
-          console.log('transactionHash', hash)
-        })
-        .on('receipt', (receipt) => {
-          console.log('receipt', receipt)
-        })
-        .on('confirmation', (confirmationNumber, receipt) => {
-          if (confirmationNumber === 0) window.alert('Transaction is confirmed!')
-        })
-        .on('error', (error) => {
-          window.alert(error.message)
-        })
-
-}
-
-/** MultiTransfer 토큰 및 최소 이체금액 지정 함수 */
-export const setTransferCondition = async (account, contractAddress, tokenAddress, minimumAmount) => {
-    
-    if (! (await checkAccount())) return alert('Please check your account!')
-
-    const contract = getContract(ABI, contractAddress)
-
-    const conditionLimit = await contract.methods.setTransferCondition(tokenAddress, minimumAmount).estimateGas({ from: account })
-
-    await contract.methods.setTransferCondition(tokenAddress, minimumAmount)
-          .send({ 
-            from: account, 
-            gas: conditionLimit 
-          })
-          .on('transactionHash', (hash) => {
-            console.log('transactionHash', hash)
-          })
-          .on('receipt', (receipt) => {
-            console.log('receipt', receipt)
-          })
-          .on('confirmation', (confirmationNumber, receipt) => {
-            if (confirmationNumber === 0) window.alert('Transaction is confirmed!')
-          })
-          .on('error', (error) => {
-            if (error.includes('caller does not have the Minter role')) window.alert('please check your Minter role!')
-            else window.alert('Transaction is failed!')
-          })
-}
-
-/** 토큰 전송 함수 (같은 금액으로 여러주소의 에어드랍) */
-export const transfer = async (account, contractAddress, amount, recipiemts) => {
-
-  if (! (await checkAccount())) return alert('Please check your account!')
-
-  const contract = getContract(ABI, contractAddress)
-
-  const transferLimit = await contract.methods.transfer(amount, recipiemts).estimateGas({ from: account })
-
-  await contract.methods.transfer(amount, recipiemts)
-        .send({
-          from: account,
-          gas: transferLimit
-        })
-        .on('transactionHash', (hash) => {
-          console.log('transactionHash', hash)
-        })
-        .on('receipt', (receipt) => {
-          console.log('receipt', receipt)
-        })
-        .on('confirmation', (confirmationNumber, receipt) => {
-          if (confirmationNumber === 0) window.alert('Transaction is confirmed!')
-        })
-        .on('error', (error) => {
-          window.alert(error.message)
-        })
-}
-
-/** 토큰 전송 함수 (여러 금액으로 여러주소의 에어드랍) */
-export const multiTransfer = async (account, contractAddress, amounts, recipiemts) => {
-
-  if (! (await checkAccount())) return alert('Please check your account!')
-
-  const contract = getContract(ABI, contractAddress)
-
-  const transferLimit = await contract.methods.transfer(amounts, recipiemts).estimateGas({ from: account })
-
-  await contract.methods.transfer(amounts, recipiemts)
-        .send({
-          from: account,
-          gas: transferLimit
-        })
-        .on('transactionHash', (hash) => {
-          console.log('transactionHash', hash)
-        })
-        .on('receipt', (receipt) => {
-          console.log('receipt', receipt)
-        })
-        .on('confirmation', (confirmationNumber, receipt) => {
-          if (confirmationNumber === 0) window.alert('Transaction is confirmed!')
-        })
-        .on('error', (error) => {
-          window.alert(error.message)
-        })
-}
-
-/** 카이카스 지갑 연동 */
-export const klaytnLogin = async (account) => {
-
-    if (window.klaytn && window.klaytn.isKaikas) {
-        const accounts = await window.klaytn.enable()
-        account = accounts[0]
-    } else if(window.confirm("Please install Kaikas wallet!")) return window.open("https://metamask.io/download.html", "_blank")
-  
-}
-
-/** 클레이튼 실시간 가스 가격 도출 함수 */
-export const getKlayGasPrice = async () => {
-    return new Promise((resolve, reject) => {
-        caver.rpc.klay.getGasPrice((err, gasPrice) => {
-            if (err) reject(err)
-            resolve(gasPrice)
-        })
-    })
-} 
-
-/** 카이카스 지갑에 연결 되 있는 상태이면 현재 주소를 가져오는 함수 */
-async function getKlayAccount() {
-  return new Promise((resolve, reject) => {
-    caver.klay.getAccounts
-      .then((accounts) => {
-        resolve(accounts[0])
-      })
-      .catch((error) => {
-        reject(error)
-      })
-  })  
-}
-
-/** 현재 카이카스 지갑이랑 연동되 있는 계정 주소랑 현재 파라미터로 받은 주소와 일치하는 지 확인하는 함수 */
-async function checkKlayAccount (account) {
-  const Account = await getKlayAccount()
-
-  if (Account.toLowerCase() !== account.toLowerCase) return false
-
-  return true
-}
-
-/** 해당 컨트랙트에 appvoe 승인 개수를 확인하는 함수 */
-async function getKlayAllowance(account, contractAddress, tokenAddress) {
-  const contract = new caver.klay.Contract(tokenABI, tokenAddress)
-  const allowance = await contract.methods.allowance(account, contractAddress).call({ from: account })
-  return allowance
-}
-
-/** 해당 토큰 컨트랙트애 approve 수행하는 함수 */
-export const setKlayApprove = async (account, contractAddress, tokenAddress, amount) => {
-
-  if (! (await checkKlayAccount())) return alert('Please check your account!')
-  
-  const contract = new caver.klay.Contract(tokenABI, tokenAddress)
-
-  const allowance = await getKlayAllowance(account, contractAddress, tokenAddress)
-
-  if (allowance >= amount) return alert('You already approved this amount!')
-  
-  const approveLimit = await contract.methods.setApprove(contractAddress, amount).estimateGas({ from: account })
-
-  const gasPrice = await getKlayGasPrice()
-
-  await contract.methods.approve(contractAddress, amount)
-        .send({
-          from: account,
-          gas: approveLimit,
-          gasPrice: gasPrice
-        })
-        .on('transactionHash', (hash) => {
-          console.log('transactionHash', hash)
-        })
-        .on('receipt', (receipt) => {
-          console.log('receipt', receipt)
-        })
-        .on('confirmation', (confirmationNumber, receipt) => {
-          if (confirmationNumber === 0) window.alert('Transaction is confirmed!')
-        })
-        .on('error', (error) => {
-          window.alert(error.message)
-        })
-
-}
-
-/** MultiTransfer 토큰 및 최소 이체금액 지정 함수 */
-export const setKlayTransferCondition = async (account, contractAddress, tokenAddress, minimumAmount) => {
-    
-  if (! (await checkKlayAccount())) return alert('Please check your account!')
-
-  const contract = new caver.klay.Contract(ABI, contractAddress)
-
-  const conditionLimit = await contract.methods.setTransferCondition(tokenAddress, minimumAmount).estimateGas({ from: account })
-
-  const gasPrice = await getKlayGasPrice()
-
-  await contract.methods.setTransferCondition(tokenAddress, minimumAmount)
-        .send({ 
-          from: account, 
-          gas: conditionLimit,
-          gasPrice: gasPrice
-        })
-        .on('transactionHash', (hash) => {
-          console.log('transactionHash', hash)
-        })
-        .on('receipt', (receipt) => {
-          console.log('receipt', receipt)
-        })
-        .on('confirmation', (confirmationNumber, receipt) => {
-          if (confirmationNumber === 0) window.alert('Transaction is confirmed!')
-        })
-        .on('error', (error) => {
-          if (error.includes('caller does not have the Minter role')) window.alert('please check your Minter role!')
-          else window.alert('Transaction is failed!')
-        })
-}
-
-/** 토큰 전송 함수 (같은 금액으로 여러주소의 에어드랍) */
-export const klayTransfer = async (account, contractAddress, amount, recipiemts) => {
-
-  if (! (await checkKlayAccount())) return alert('Please check your account!')
-
-  const contract = new caver.klay.Contract(ABI, contractAddress)
-
-  const transferLimit = await contract.methods.transfer(amount, recipiemts).estimateGas({ from: account })
-
-  const gasPrice = await getKlayGasPrice()
-
-  await contract.methods.transfer(amount, recipiemts)
-        .send({
-          from: account,
-          gas: transferLimit,
-          gasPrice: gasPrice
-        })
-        .on('transactionHash', (hash) => {
-          console.log('transactionHash', hash)
-        })
-        .on('receipt', (receipt) => {
-          console.log('receipt', receipt)
-        })
-        .on('confirmation', (confirmationNumber, receipt) => {
-          if (confirmationNumber === 0) window.alert('Transaction is confirmed!')
-        })
-        .on('error', (error) => {
-          window.alert(error.message)
-        })
-}
-
-/** 토큰 전송 함수 (여러 금액으로 여러주소의 에어드랍) */
-export const klayMultiTransfer = async (account, contractAddress, amounts, recipiemts) => {
-
-  if (! (await checkKlayAccount())) return alert('Please check your account!')
-
-  const contract = new caver.klay.Contract(ABI, contractAddress)
-
-  const transferLimit = await contract.methods.transfer(amounts, recipiemts).estimateGas({ from: account })
-
-  const gasPrice = await getKlayGasPrice()
-
-  await contract.methods.transfer(amounts, recipiemts)
-        .send({
-          from: account,
-          gas: transferLimit,
-          gasPrice: gasPrice
-        })
-        .on('transactionHash', (hash) => {
-          console.log('transactionHash', hash)
-        })
-        .on('receipt', (receipt) => {
-          console.log('receipt', receipt)
-        })
-        .on('confirmation', (confirmationNumber, receipt) => {
-          if (confirmationNumber === 0) window.alert('Transaction is confirmed!')
-        })
-        .on('error', (error) => {
-          window.alert(error.message)
-        })
-}
 
  export default web3
